b8bf145224b677430a75e137ad9d70d8d4f9e065
=arty542 <=abidsaleha05@gmail.com>
2025-03-27 03:11:37 +0500

    partial CI: added react for frontend

--- phishing/admin.py
+++ phishing/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import TargetUser, PhishingEmail, ClickTracking
+from .models import EmailTracking
 
-admin.site.register(TargetUser)
-admin.site.register(PhishingEmail)
-admin.site.register(ClickTracking)
+class EmailTrackingAdmin(admin.ModelAdmin):
+    list_display = ('user_email', 'test_date', 'clicked_link', 'passed_test')  # Show these columns
+    search_fields = ('user_email',)  # Add a search bar for emails
+    list_filter = ('clicked_link', 'passed_test')  # Add filter options
+
+admin.site.register(EmailTracking, EmailTrackingAdmin)  # Register with customization
